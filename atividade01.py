e86e120a45cde6f51f29ab0345d623a52e8884a7
Samuel Eiji Natume <163770214+SamuelNatume@users.noreply.github.com>
2024-03-27 12:36:23 -0300

    Update atividade01.py

--- atividade01.py
+++ atividade01.py
@@ -10,3 +10,47 @@ elif x == 2:
 
 else:
     print("ERRO")
+
+
+
+
+
+# Programa para verificar se número são pares ou ímpares 
+# O programa deve parar caso sejam informados 5 números pares ou 
+# Caso a soma dos ímpares informados passe de 30
+# Vamos informar as somas e as quantidades 
+# utilizar "break e continue"
+
+numPares = 0
+numImpares = 0
+somaPares = 0
+somaImpares = 0
+
+
+while True:
+    num = int(input("DIgite um número inteiro e positivo: "))
+    if num < 0:
+        print("O número não pode ser negativo.")
+        continue
+    if num % 2 == 0:
+        numPares += 1   
+        somaPares += num
+    else:
+        numImpares += 1
+        somaImpares += num
+    if numPares == 5 or somaImpares > 30:
+        break
+    
+print(f"Soma dos pares: {somaPares}")
+print(f"Número de pares: {numPares}")
+
+print(f"Soma dos ímpares: {somaImpares}")
+print(f"Número de ímpares: {numImpares}")
+
+
+
+
+
+
+
+  
